472d58245f37d67f06f6695095e234bf35ed7297
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2019-10-03 16:27:11 +0300

    update(*): add functionality

--- extension.js
+++ extension.js
@@ -1,4 +1,19 @@
 const vscode = require('vscode');
+const fs = require('fs');
+const ignoreWorkspace = [
+	'app/data/kpi/*',
+	'app/media/**',
+];
+
+function getFileNameFromPath(filePath) {
+	return filePath.split("/").pop().replace(/\.[^.]*$/, '');
+}
+function parseJsData(filename) {
+	let json = fs.readFileSync(filename, 'utf8')
+		.replace(/\s*\/\/.+/g, '')
+		.replace(/,(\s*\})/g, '}')
+	return JSON.parse(json);
+}
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -6,31 +21,48 @@ const vscode = require('vscode');
 
 
 function activate(context) {
-	function getFileNameFromPath(path) {
-		return path.split("/").pop().replace(/\.[^.]*$/, '');
-	}
 
 	let disposable = vscode.commands.registerCommand('hrod.open', function () {
 		const path = vscode.workspace.rootPath;
-		const filepath = vscode.window.activeTextEditor.document.fileName;
-		const fileName = getFileNameFromPath(filepath);
-
-		console.log(fileName);
+		const structure = parseJsData(`${path}/structure.json`);
 
 		vscode.env.clipboard.readText().then((slideId) => {
-			if (!slideId) return;
+			console.log();
+			
+			if (Object.keys(structure.slides).some(item => item === slideId)) {
+				vscode.workspace.findFiles(
+					new vscode.RelativePattern(path, `{app/**/${slideId}.*}`),
+					new vscode.RelativePattern(path, `{${ignoreWorkspace.join(',')}}`),
+				)
+					.then((TextDocuments) => {
 
-			vscode.workspace.findFiles(`app/**/${slideId}.*`, path, 4)
-				.then((TextDocuments) => {
-
-					TextDocuments.forEach(document => {
-						vscode.workspace.openTextDocument(document)
-
-							.then((document) => {
-								vscode.window.showTextDocument(document, vscode.ViewColumn.One, true);
-							});
+						TextDocuments.forEach(document => {
+							vscode.workspace.openTextDocument(document)
+			
+								.then((document) => {
+									vscode.window.showTextDocument(document, { preview: false });
+								});
+						});
 					});
-				});
+			} else {
+				const filePath = vscode.window.activeTextEditor.document.fileName;
+				const fileName = getFileNameFromPath(filePath);
+				vscode.workspace.findFiles(
+					new vscode.RelativePattern(path, `{app/**/${fileName}.*}`),
+					new vscode.RelativePattern(path, `{${ignoreWorkspace.join(',')}}`),
+				)
+					.then((TextDocuments) => {
+
+						TextDocuments.forEach(document => {
+							vscode.workspace.openTextDocument(document)
+			
+								.then((document) => {
+									vscode.window.showTextDocument(document, { preview: false });
+								});
+						});
+					});
+			}
+			
 		});
 	});
 
