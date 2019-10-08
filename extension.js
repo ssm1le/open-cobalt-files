576ded6b62b18e40aac9fcf683cee85f4d3a2ed7
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2019-10-08 12:18:10 +0300

    update(*): refactoring, fix open currect localization

--- extension.js
+++ extension.js
@@ -2,11 +2,15 @@ const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 
-const ignoreWorkspace = [
-	'app/data/kpi/*',
-	'app/media/**',
-];
-
+function getWorkspace(slideId, settings) {
+	return [
+		`app/${slideId}.html`,
+		`app/styles/${slideId}.css`,
+		`app/data/models/${slideId}.json`,
+		`app/i18n/${settings.lang}/${slideId}.json`,
+		`app/controllers/${slideId}.js`,
+	]
+}
 function getFileNameFromPath(filePath) {
 	const fileName = path.parse(filePath)
 	return fileName.name;
@@ -24,10 +28,9 @@ function getPathToPresentation(filePath) {
 	return filePath.split(`${path.sep}${folderName}${path.sep}`).pop();
 }
 
-function openFiles(rootPath, slideId) {
+function openFiles(rootPath, slideId, settings) {
 	vscode.workspace.findFiles(
-		new vscode.RelativePattern(rootPath, `{app/**/${slideId}.*}`),
-		new vscode.RelativePattern(rootPath, `{${ignoreWorkspace.join(',')}}`),
+		new vscode.RelativePattern(rootPath, `{${getWorkspace(slideId, settings).join(',')}}`)
 	)
 		.then((TextDocuments) => {
 			TextDocuments.forEach(document => {
@@ -62,13 +65,14 @@ function activate(context) {
 		const rootPath = vscode.workspace.rootPath;
 		const presentationPath = getPathToPresentation(rootPath);
 		const structure = parseJsData(`${rootPath}/structure.json`);
+		const settings = parseJsData(`${rootPath}/app/settings/app.json`);
 
 		vscode.env.clipboard.readText()
 			.then((slideId) => {
 				if (Object.keys(structure.slides).some(item => item === slideId)) {
 					const chapter = getChapter(structure.chapters, slideId);
 
-					openFiles(rootPath, slideId);
+					openFiles(rootPath, slideId, settings);
 					if (chapter) {
 						openUrl(presentationPath, chapter, slideId);
 					}
@@ -79,7 +83,7 @@ function activate(context) {
 					const fileName = getFileNameFromPath(filePath);
 					const chapter = getChapter(structure.chapters, fileName);
 
-					openFiles(rootPath, fileName);
+					openFiles(rootPath, fileName, settings);
 					if (chapter) {
 						openUrl(presentationPath, chapter, fileName);
 					}
