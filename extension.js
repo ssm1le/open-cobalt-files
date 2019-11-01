9d3612845435fa8d6e4f8604236c1b179b686146
Alex Hrodetskiy <a.hrodetskiy@viseven.com>
2019-11-01 17:43:52 +0200

    update(*): add parse json

--- extension.js
+++ extension.js
@@ -1,48 +1,53 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const open = require('./open-files.js');
-// const html = require('./html.js');
 
 const cheerio = require('cheerio');
-
+const path = require('path');
 /**
  * @param {vscode.ExtensionContext} context
- **/
+ */
 
-function writeModel(modelId, path, slideName) {
-	const json = `${path}/app/data/models/${slideName}.json`;
-	const results = {
-		${modelId}: {
-			"html": t.${modelId}
-		}
-	}
-	fs.writeFileSync(json, "Привет ми ми ми!");
-}
-function someFunc(element,slideName) {
-	const rootPath = vscode.workspace.rootPath;
-	let model = element.attribs.model;
-	let name = element.name;
-let id = element.attribs.idl;
-	if(name==="co-text"){
-		writeModel(id,rootPath,slideName);
-	}
-
-}
-function getTagsOnSlides(document) {
-	let file = fs.readFileSync(document.fileName, 'utf8');
-	const $ = cheerio.load(file)
-	let tags = [];
-	$('article').find('*').each((i, elem) => {
-		tags.push(elem)
-	});
-	return tags;
-}
 function activate(context) {
+
+	function getModelObj(modelId) {
+		// const json = `${path}/app/data/models/${slideName}.json`;
+		return {
+			[modelId]: {
+				html: `t.${modelId}`
+			}
+		};
+	}
+	function getModelForTag(element) {
+		let name = element.name;
+		let id = element.attribs.id;
+		let model = element.attribs.model;
+
+
+
+		if (id && name == "co-text") {
+			console.log("done");
+
+			return getModelObj(id);
+		}
+		return {};
+
+	}
+	function getTagsOnSlides(document) {
+		let file = fs.readFileSync(document.fileName, 'utf8');
+		const $ = cheerio.load(file)
+		let tags = [];
+		$('article').find('*').each((i, elem) => {
+			tags.push(elem)
+		});
+		return tags;
+	}
 
 	const subscriptions = [
 		vscode.commands.registerCommand("hrod.open", open.openCobaltFiles),
 		vscode.commands.registerCommand("hrod.model", () => {
-			
+
+			const rootPath = vscode.workspace.rootPath;
 			const activeDocument = vscode.window.activeTextEditor.document;
 			const fileName = open.getFileNameFromPath(activeDocument.fileName);
 
@@ -50,9 +55,15 @@ function activate(context) {
 				vscode.window.showInformationMessage('Open the file with the HTML extension');
 			} else {
 				const tags = getTagsOnSlides(activeDocument);
+				let tempModelObj = {};
 				tags.forEach(elem => {
-					someFunc(elem,fileName);
+					tempModelObj = Object.assign(tempModelObj, getModelForTag(elem))
 				});
+				let jsonModel = fs.readFileSync(`${rootPath}/app/data/models/${fileName}.json`, 'utf8');
+				jsonModel = JSON.parse(jsonModel)
+				jsonModel = Object.assign(jsonModel, tempModelObj);
+				
+				fs.writeFileSync(`${rootPath}/app/data/models/${fileName}.json`, JSON.stringify(jsonModel));
 			}
 		}),
 		vscode.commands.registerCommand("hrod.fix", () => { })
